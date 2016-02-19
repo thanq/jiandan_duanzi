ef9af61130958f6b31d300f8988f02f212cdcb0f
qinqiangqiang <thanq1@gmail.com>
2016-02-19 12:26:27 +0800

    update

--- run_jiandan_duanzi_webapp.py
+++ run_jiandan_duanzi_webapp.py
@@ -1,5 +1,10 @@
+
+import os
 from jiandan_web import app
 
 if __name__ == '__main__':
-    app.run()
+    if os.environ.get('PRODUCTION') != "PRODUCTION":
+        app.run()
+    else:
+        app.run('0.0.0.0')
 
