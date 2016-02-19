3223b2188b2dd23f20cd676b7bbb2db520590ce6
qinqiangqiang <thanq1@gmail.com>
2016-02-19 12:24:23 +0800

    update

--- jiandan_web/__init__.py
+++ jiandan_web/__init__.py
@@ -7,6 +7,10 @@ import jd_web_config as config
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(config.DevelopmentConfig)
+
+if os.environ.get('PRODUCTION') != "PRODUCTION":
+    app.config.from_object(config.DevelopmentConfig)
+else:
+    app.config.from_object(config.ProductionConfig)
 
 import jiandan_web.controller.root_controller
